71762ab141f94d8984230ae8640c1a1949aeb466
Viktoria Ihnatsik <ignatik_viktoria@hotmail.com>
2021-01-12 14:50:35 +0100

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import CustomerListPage from './pages/CustomerListPage';
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hej</p>
+
+      <Switch>
+
+        <Route path="/home">
+          <CustomerListPage />
+        </Route>
+
+
+        <Route path="/">
+          <LoginPage />
+        </Route>
+
+      </Switch>
+
     </div>
   );
 }
