d106dcbbee1c62df8f5252043b33cf965ded3d39
Viktoria Ihnatsik <ignatik_viktoria@hotmail.com>
2021-01-12 15:34:53 +0100

    Add compon CustomerListItem

--- src/pages/CustomerListPage.jsx
+++ src/pages/CustomerListPage.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import CustomerListItem from '../components/CustomerListItem'
 
 export default function CustomerListPage() {
+
+  const [customerList, setCustomerList] = useState([]) // variabel för funkt getCustomerList; [] -> resultatet kommer från array, darför skriver vi []
+
+  useEffect ( () => {
+    getCustomerList()  // när sida randeras få customer list
+  }, [])
+
+
+  // hämta list med alla kundar
+  function getCustomerList() {
+    const url = "https://frebi.willandskill.eu/api/v1/customers/"
+    const token = localStorage.getItem("WEBB20") // använda token som fi vick från respons
+     fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      }
+    })
+    .then(res => res.json())
+    .then(data => setCustomerList(data.results))
+  }
   return (
     <div>
       <p>Customer List Page</p>
+      {customerList.map(item => {   // mapa över 
+        return <CustomerListItem key={item.id} customerData={item} />
+      })}
     </div>
   )
 }
