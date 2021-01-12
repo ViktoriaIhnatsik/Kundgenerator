529a9c493b7903dc0ecef3857cd4b0377e82b157
Viktoria Ihnatsik <ignatik_viktoria@hotmail.com>
2021-01-12 22:21:01 +0100

    Add CustomerUppdatePage

---
+++ src/pages/CustomerUpdatePage.jsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect} from 'react'
+import { useHistory } from 'react-router-dom'
+
+export default function CustomerUpdatePage(props) {
+  const customerId = props.match.params.id // id ligger här -> console.log(props)
+  const [formData, setFormData] = useState({})
+  const history = useHistory()
+
+  function getCustomerItem() {
+   const url = `https://frebi.willandskill.eu/api/v1/customers/${customerId}/` //för att få info för en customer
+    const token = localStorage.getItem("WEBB20")
+    fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      }
+    })
+    .then(res => res.json())
+    .then(data => setFormData(data))
+ }
+
+  useEffect (() => {
+   getCustomerItem()
+ }, [])
+
+ function handleOnChange(e) {
+   const name = e.target.name  //kollar namn 
+   const value = e.target.value  //kolar input vad man skriv
+   const newObj = {...formData, [name]: value} //uppdatera varje gång med ny info
+   setFormData(newObj)
+ }
+
+   function renderInput(name, label, type) {
+    return (
+    <div>
+     <label>{label}</label>
+      <input 
+       type={type || "text"} 
+       name={name} 
+       value={formData[name] || ""}  // kollar om vi hinner göra fetch så sätter formData, om inte retunerar tom sträng
+       onChange={handleOnChange}
+    />
+    </div>
+    )
+  }
+
+   function handleOnSubmit(e) {
+    e.preventDefault()
+    const url = `https://frebi.willandskill.eu/api/v1/customers/${customerId}/` 
+    const token = localStorage.getItem("WEBB20")
+    fetch(url, {
+      method: "PUT", // När man vill förändra hela objektet
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      }
+    })
+    .then(res => res.json())
+    .then(data => history.push(`/customers/${customerId}`))
+
+  }
+  return (
+    <div>
+     <p>Update Customer</p> 
+     <form onSubmit={handleOnSubmit}>
+        {renderInput("name", "Customer Name")}
+        {renderInput("email", "Customer Email")}
+        {renderInput("organisationNr", "Organisation Number")}
+        {renderInput("paymentTerm", "Payment Term", "number")}
+        {renderInput("phoneNumber", "Phone Number", "tel")}
+        {renderInput("reference", "Reference")}
+        {renderInput("vatNr", "Vat Number")}
+        {renderInput("website", "Website", "url")}
+        <button type="submit">Update Customer</button>
+      </form>
+    </div>
+  )
+}
